c6d3f26567b3fa03e28411a582cd7b52243d9977
aosman <pianogreen0@gmail.com>
2023-02-27 10:36:39 +0000

    adding event listner

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ window.addEventListener('load',function(){
 const loading = document.getElementById('loading')
 loading.style.display = 'none'
 const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d')
+canvas.width = window.innerHeight
 })
