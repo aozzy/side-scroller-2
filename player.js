459d0a9566e4637913193cca2c6d177b0e62c441
aosman <pianogreen0@gmail.com>
2023-03-03 23:53:45 +0000

    player class

--- player.js
+++ player.js
@@ -1,10 +1,13 @@
 class Player {
-  constructor(gameWidth,gameHeight){
-    this.gameWidth = gameWidth
-  this.gameHeight = gameHeight
-  this.state = []
-  }
-  hello(){
-    console.log(this.state);
+  constructor(gameWidth, gameHeight) {
+    this.gameWidth = gameWidth;
+    this.gameHeight = gameHeight;
+    this.state = [];
+    this.currentState = currentState;
+    this.image = document.getElementById("dogImage");
+    this.width = 200;
+    this.height = 181.83;
+    this.x = 0;
+    this.y = 0
   }
 }
