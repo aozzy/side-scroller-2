727b9495ea0bbaf713d290eae74ff0eb00606b7b
aosman <pianogreen0@gmail.com>
2023-03-04 11:58:23 +0000

    player class

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const loading = document.getElementById('loading')
 loading.style.display = 'none'
 const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
-canvas.width = window.innerHeight
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+const player = new Player(canvas.width,canvas.height)
+console.log(player);
 })
