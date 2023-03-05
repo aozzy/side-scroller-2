33f95f3a4d258d2a78b719cb57a836a4df3a3b76
aosman <pianogreen0@gmail.com>
2023-03-05 12:08:31 +0000

    player class

--- player.js
+++ player.js
@@ -7,10 +7,11 @@ export default class Player {
     this.image = document.getElementById("dogImage");
     this.width = 200;
     this.height = 181.83;
-    this.x = 100;
-    this.y = 200
+    this.x = 0;
+    this.y = 0
+    this.frameX = 0
   }
   draw(context){
-    context.drawImage(this.image,0,0,this.width,this.height,this.x,this.y,this.width,this.height)
+    context.drawImage(this.image,0,0,this.width * 3,this.height * 6,this.width,this.height,this.x,this.y,this.width,this.height)
   }
 }
