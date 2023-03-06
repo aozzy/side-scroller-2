9e4349f04f33f3e6841b31938282cb51132eb5d5
aosman <pianogreen0@gmail.com>
2023-03-06 11:45:08 +0000

    inputhandler class

--- input.js
+++ input.js
@@ -2,7 +2,16 @@ export default class Inputhandler{
   constructor(){
     this.lastKey = ''
     window.addEventListener('keydown',function(e){
-      
+      switch(e.key){
+        case 'ArrowLeft':
+          this.lastKey = 'PRESS left'
+      }
+    })
+    window.addEventListener('keyup',function(e){
+      switch(e.key){
+        case 'ArrowLeft':
+          this.lastKey = 'RELEASE left'
+      }
     })
   }
 
