a51791080f1c4b51e6f1c96fc86a02d729f35e7c
aosman <pianogreen0@gmail.com>
2023-03-18 23:52:27 +0000

    player.js

--- player.js
+++ player.js
@@ -15,5 +15,7 @@ export default class Player {
   draw(context){
     context.drawImage(this.image,0,0,this.width * 3,this.height * this.frameY,this.width,this.height,this.x,this.y,this.width,this.height)
     console.log(this.state);
+    console.log(this.frameX);
+    console.log(this.frameY);
   }
 }
