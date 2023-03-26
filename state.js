b58f89eb6638f704f9b766f052139bec056a93fd
aosman <pianogreen0@gmail.com>
2023-03-26 13:01:37 +0100

    state.js

--- state.js
+++ state.js
@@ -52,11 +52,13 @@ export class StandingRight extends State{
   }
   handleInput(input){
     if (input === 'PRESS left'){
-      this.player.setState(states.STANDING_LEFT)
-    } else if (input === "PRESS down"){
+      this.player.setState(states.RUNNING_LEFT)
+    } else if (input === "PRESS right"){
+      this.player.setState(states.RUNNING_RIGHT)
+    }
+    else if (input === "PRESS down"){
       this.player.setState(states.SITTING_RIGHT)
     }
-
   }
 }
 
