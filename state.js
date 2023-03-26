d742f72704d5c1f703274f25421436bfbb87e70a
aosman <pianogreen0@gmail.com>
2023-03-26 11:47:51 +0100

    state.js

--- state.js
+++ state.js
@@ -3,6 +3,8 @@ export const states = {
   STANDING_RIGHT: 1,
   SITTING_LEFT:2,
   SITTING_RIGHT:3,
+  RUNNING_LEFT:4,
+  RUNNING_RIGHT:5,
   
 
 }
@@ -76,6 +78,27 @@ export class SittingLeft extends State{
 export class SittingRight extends State{
   constructor(player){
     super("SITTING Right")
+    this.player = player
+  }
+  enter(){
+    this.player.frameY = 8
+
+  }
+  handleInput(input){
+    if (input === 'PRESS left'){
+      this.player.setState(states.SITTING_LEFT)
+    } else if (input === 'RELEASE down'){
+      this.player.setState(states.STANDING_RIGHT)
+    }
+
+
+  }
+}
+
+
+export class RunningLeft extends State{
+  constructor(player){
+    super("RUNNING LEFT")
     this.player = player
   }
   enter(){
